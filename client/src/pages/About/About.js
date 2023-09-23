d3b5fe50522febbbcd00db6ad46a1a9c499a40f1
Umair513 <soomroumair513@gmail.com>
2023-09-23 21:35:26 +0500

    Projects Section

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -12,7 +12,12 @@ const About = () => {
                     </div>
                     <div className="col-md-6 about-content col-xl-6 col-lg-6 col-xs-12">
                         <h1>About me</h1>
-                        <p>I am a Computer Science graduate from SUKKUR IBA University (2019-2023). I have expertise in Web Development and want to establish my career in this field. My Expertise is MERN STACK.</p>
+                        <p>Innovative and motivated Computer Science graduate skilled in Web Engneering computer science
+                            graduate adept in diverse domains including web and Android development. Proficient in an array of
+                            technologies such as JavaScript, React JS, Node JS, Git, MongoDB, React-Native, and Python. I led
+                            the creation of an intuitive an inter-campus socializing app for students (Student Collab), Chat
+                            Application, Budget Calculator, Pizza Shop, Food Recipe App, Text Utils App. With a proven record of
+                            delivering high-impact projects, I am primed to contribute effectively and drive innovation in your team.</p>
                     </div>
                 </div>
             </div>
--- client/src/pages/Techstack/Techstack.js
+++ client/src/pages/Techstack/Techstack.js
@@ -6,7 +6,7 @@ const Techstack = () => {
     return (
         <>
             <div className="container techstack">
-                <h2 className='col-12 mt-5 mb-1 text-center'>Technologies Stack</h2>
+                <h2 className='col-12 mt-5 mb-1 text-center text-uppercase'>Technologies Stack</h2>
                 <hr></hr>
                 <p className='pb-3 text-center'>👉 including programming languages, frameworks, databases, front-end and back-end tools, and APIs</p>
 
